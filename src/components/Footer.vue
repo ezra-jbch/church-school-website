d74558ff434b801d8a9071c12a0fbbf4d5309d0d
Ezra Freeman <ezra.jbch@gmail.com>
2021-07-16 18:44:19 -0500

    footer component made

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+
+  </div>
+</template>
+
+<style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100px;
+        background: #666666;
+        padding: 15px 15px 10px;
+    }
+</style>
