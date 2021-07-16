f650ef112a235a734377b64b4400c358906bb717
Ezra Freeman <ezra.jbch@gmail.com>
2021-07-15 23:07:56 -0500

    installed and imported primevue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
+// Vue
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// PrimeVue
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css'
+import 'primeicons/primeicons.css'
+/* import 'primevue/resources/primevue.min.css' */
+
+// PrimeVue components (import more components here as you see fit)
+import DataTable from 'primevue/datatable'
+
+// Vue SPA (single page application)
+const app = createApp(App)
+
+// initialize app with plugins
+app.use(router)
+app.use(PrimeVue)
+
+// add PrimeView components (add more components here as you see fit)
+app.component("DataTable", DataTable)
+
+// mount app in index.html
+app.mount('#app')
