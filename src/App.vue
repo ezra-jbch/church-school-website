e79a4a762fd15bca0fe2e3301d6adf437ed2cfd0
Daniel Kweon <danielkweon19@gmail.com>
2021-07-17 12:13:37 -0500

    basic dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <Header />
+  <!--
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
+  -->
+  <Dropdown/>
   <Footer />
 </template>
 
@@ -12,8 +15,9 @@
 
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import Dropdown from './components/Dropdown.vue'
 export default{
-  components: { Header, Footer}
+  components: { Header, Footer, Dropdown}
 }
 </script>
 <style>
