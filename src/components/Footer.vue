0855b98363ce1632a50cbd73d93080346313c360
Ezra Freeman <ezra.jbch@gmail.com>
2021-07-19 20:36:53 -0500

    copyright added and reformatted spacing

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,20 @@
 <template>
-     <div class="footer">
+    <div class="footer">
+        <p class="footer-text left">Jesus Baptist U.S.A. Conference © {{ currentYear }} All rights reserved</p>
         <router-link class="footer-text" :to="{ name: 'ContactUs' }">Contact Us</router-link>
-     </div>
+    </div>
 </template>
+
+<script>
+export default ({
+    computed: {
+        currentYear() {
+            return new Date().getFullYear()
+        }
+    }
+})
+</script>
+
 
 <style>
     .footer {
@@ -10,15 +22,20 @@
         left: 0;
         bottom: 0;
         width: 100%;
-        height: 100px;
         background: #333333;
-        padding: 15px 15px 10px;
-
+        padding: 15px 30px;
+        display: flex;
     }
     .footer-text {
         color: #808080;
         text-decoration: none;
         font-size: 15px;
         font-family: "Open Sans", sans-serif;
+        margin: 0;
+        display: inline
+    }
+    .left {
+        margin-right: auto;
+        padding: 0 30px 0 0;
     }
 </style>
