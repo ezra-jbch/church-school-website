26ec212065f5a0d3fa3ed1b23e3f3595812f8d20
Daniel Kweon <danielkweon19@gmail.com>
2021-07-22 00:14:44 -0500

    added login button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,68 @@
 <template>
-    <header class = "header">
-        <router-link :to="{ name: 'Home' }"> <!-- Routes the page back to the Home page when you press on the Logo -->
-            <img alt ="church school logo" src = "../assets/logo.png" class = "img">
-        </router-link>
-    </header>
+  <header class="header">
+    <router-link :to="{ name: 'Home' }" id="router-link">
+      <!-- Routes the page back to the Home page when you press on the Logo -->
+      <img alt="church school logo" src="../assets/logo.png" class="img" />
+    </router-link>
+    <div class ="button-pos">
+      <button class="btn btn-outline-secondary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-box-arrow-in-right"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"
+          />
+          <path
+            fill-rule="evenodd"
+            d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+          />
+        </svg>
+        Login
+      </button>
+    </div>
+  </header>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
+
 <style>
-    .header{
-        padding: 10px;
-        background: #f8f8f8;
-        text-align: left;
-    }
-    .img{
-        width: 30%;
-        margin-left: 1.5%;
-        /*Re-renders image so that it is easier to code*/
-        -ms-transform: translateZ(0); /* IE 9 */
-        -webkit-transform: translateZ(0); /* Chrome, Safari, Opera */
-        transform: translateZ(0);
-    }
-    @media only screen and (max-width: 480px) {
-    /* For mobile phones: */
-        .header{
-            text-align: center;
-        }
-        .img{
-            width: 80%;
-        }
-    }
+.button-pos{
+    margin-right: 2%;
+}
+#router-link {
+  display: inline-block;
+  width: 30%;
+  margin-left: 1%;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f8f8f8;
+  padding: 10px;
+}
+.img {
+  width: 100%;
+  /*Re-renders image so that it is easier to code*/
+  -ms-transform: translateZ(0); /* IE 9 */
+  -webkit-transform: translateZ(0); /* Chrome, Safari, Opera */
+  transform: translateZ(0);
+}
+@media only screen and (max-width: 480px) {
+  /* For mobile phones: */
+  .header {
+    text-align: center;
+  }
+  .img {
+    width: 80%;
+  }
+}
 </style>
