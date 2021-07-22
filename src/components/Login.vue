a6527dacad01791ac74ddcf3e13cd708312bf810
Daniel Kweon <danielkweon19@gmail.com>
2021-07-22 18:18:00 -0500

    added modal to button

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="backdrio">
-      <h1>Modal</h1>
+  <div class="backdrop">
+      <div class ="modalBox">
+          <h1>Modal</h1>
+      </div>
   </div>
 </template>
 
@@ -11,5 +13,18 @@ export default {
 </script>
 
 <style>
-
+.modalBox{
+    width: 400px;
+    padding: 100px;
+    margin: 300px auto;
+    background: white;
+    border-radius: 10px;
+}
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
 </style>
