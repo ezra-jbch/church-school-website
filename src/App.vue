91c6060fd976293b17a8b60b7ff155ea58218378
Daniel Kweon <danielkweon19@gmail.com>
2021-07-22 17:56:28 -0500

    made login component, made on function for button

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,55 @@
 <template>
-  <Header />
-  <Dropdown :pages="pageArray"/>
-  <router-view v-slot="{Component}"> <!--This is how you do transitions between routes-->
-    <transition name ="route" mode="out-in">
-      <component :is=Component></component>
-    </transition>  
+  <Header @seeLogin="isVisible"/> 
+  <div v-if="showLogin">
+    <Login/>
+  </div>
+  <Dropdown :pages="pageArray" />
+  <router-view v-slot="{ Component }">
+    <!--This is how you do transitions between routes-->
+    <transition name="route" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
   </router-view>
-  
   <Footer />
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Dropdown from "./components/Dropdown.vue";
+import Login from "./components/Login.vue";
 
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import Dropdown from './components/Dropdown.vue'
-export default{
-  components: { Header, Footer, Dropdown},
+export default {
+  components: { Header, Footer, Dropdown, Login},
   data() {
-    return{
+    return {
       pageArray: [
-        {page: 'Kindergarten', route: "Kindergarten"},
-        {page: 'Elementary' , route: "Elementary"},
-        {page: 'Youth Group', route: "Youth-Group"},
-        ]
+        { page: "Kindergarten", route: "Kindergarten" },
+        { page: "Elementary", route: "Elementary" },
+        { page: "Youth Group", route: "Youth-Group" },
+      ],
+      showLogin: false
+    };
+  },
+  methods:{
+    isVisible(){
+      this.showLogin = true;
+      console.log( this.showLogin);
     }
   }
-}
+};
 </script>
 <style>
 /*Logo hex values:
 
 Dark blue: #005595
 Lighter blue: #36b4e5
-*/ 
-html,body{
+*/
+html,
+body {
   /*Fixed Margins of HTML Page*/
-  margin:0;
-  padding:0;
+  margin: 0;
+  padding: 0;
   overflow-x: clip; /*Need this to prevent overflow. Did not use 'overflow-x: hidden' because it breaks dropdown*/
 }
 #app {
@@ -60,18 +72,18 @@ html,body{
   color: #42b983;
 }
 /*route transitions*/
-.route-enter-from{
-  opacity:0;
+.route-enter-from {
+  opacity: 0;
   transform: translateX(100px);
 }
-.route-enter-active{
+.route-enter-active {
   transition: all 0.2s ease-out;
 }
-.route-leave-to{
+.route-leave-to {
   opacity: 0;
   transform: translateX(-100px);
 }
-.route-leave-active{
+.route-leave-active {
   transition: all 0.2s ease-out;
 }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="backdrio">
+      <h1>Modal</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
