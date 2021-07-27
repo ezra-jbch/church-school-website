80bd0d1c7dc69bb1d21e73137c02ebf46ece2ab7
Daniel Kweon <danielkweon19@gmail.com>
2021-07-27 17:08:17 -0500

    added table to add three pages

--- src/views/YouthGroup.vue
+++ src/views/YouthGroup.vue
@@ -1,72 +1,38 @@
 <template>
   <div class="root">
     <h1>Youth Group</h1>
-    <div class="container" id="modalBox">
-      <!-- code for search: https://mdbootstrap.com/docs/standard/forms/search/ -->
-      <div class="input-group rounded" id="searchBar">
-        <input
-          type="search"
-          class="form-control rounded"
-          placeholder="Search"
-          aria-label="Search"
-          aria-describedby="search-addon"
-        />
-        <span class="input-group-text border-0" id="search-addon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-search"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
-            />
-          </svg>
-        </span>
-      </div>
-      <table class="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">Date</th>
-            <th scope="col">Chapter</th>
-            <th scope="col">Title</th>
-            <th scope="col">PDF</th>
-            <th scope="col">Sermon</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">01/03</th>
-            <td>1</td>
-            <td>Daily Bible Study</td>
-            <td><button>Download</button></td>
-            <td><button>Watch</button></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <TableBox :array1="arrayBox" />
   </div>
 </template>
 
 <script>
+import TableBox from "../components/TableBox.vue"
 export default {
+  components:{TableBox},
   data() {
-    return {};
+    return {
+      arrayBox: [
+        {
+          id: "1",
+          date: "01/03",
+          title: "Daily Bible Study",
+          pdf: "",
+          sermon:
+            "https://www.youtube.com/watch?v=3fyTOiK29nM&list=PLWj53mXE8Z2SGZVwNEZLbWkllo1KrC4lv&index=1&ab_channel=JBChurchSchool",
+        },
+        {
+          id: "2",
+          date: "01/10",
+          title: "In This Manner Pray",
+          pdf: "",
+          sermon:
+            "https://www.youtube.com/watch?v=swizswbN8zw&list=PLWj53mXE8Z2SGZVwNEZLbWkllo1KrC4lv&index=3&ab_channel=JBChurchSchool",
+        },
+      ],
+    };
   },
 };
 </script>
 
 <style>
-#modalBox {
-  background-color: white;
-  margin-top: 2%;
-  border: 1px solid black;
-}
-#searchBar {
-  width: 20%;
-  margin-top: 1%;
-  float: right;
-}
 </style>
