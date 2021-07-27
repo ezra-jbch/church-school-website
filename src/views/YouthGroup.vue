cb4eaa425c93e60884610d7feb0539c9d3615203
Daniel Kweon <danielkweon19@gmail.com>
2021-07-27 14:42:17 -0500

    Added search function

--- src/views/YouthGroup.vue
+++ src/views/YouthGroup.vue
@@ -2,6 +2,30 @@
   <div class="root">
     <h1>Youth Group</h1>
     <div class="container" id="modalBox">
+      <!-- code for search: https://mdbootstrap.com/docs/standard/forms/search/ -->
+      <div class="input-group rounded" id="searchBar">
+        <input
+          type="search"
+          class="form-control rounded"
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="search-addon"
+        />
+        <span class="input-group-text border-0" id="search-addon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+            />
+          </svg>
+        </span>
+      </div>
       <table class="table table-hover">
         <thead>
           <tr>
@@ -20,20 +44,6 @@
             <td><button>Download</button></td>
             <td><button>Watch</button></td>
           </tr>
-          <tr>
-            <th scope="row">01/10</th>
-            <td>2</td>
-            <td>In This Manner Pray</td>
-            <td><button>Download</button></td>
-            <td><button>Watch</button></td>
-          </tr>
-          <tr>
-            <th scope="row">01/17</th>
-            <td>3</td>
-            <td>The Fellowship of Christians</td>
-            <td><button>Download</button></td>
-            <td><button>Watch</button></td>
-          </tr>
         </tbody>
       </table>
     </div>
@@ -49,9 +59,14 @@ export default {
 </script>
 
 <style>
-
 #modalBox {
   background-color: white;
   margin-top: 2%;
+  border: 1px solid black;
+}
+#searchBar {
+  width: 20%;
+  margin-top: 1%;
+  float: right;
 }
 </style>
