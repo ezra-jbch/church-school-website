b193c203c5fb2a5cb1f5284be8c118127cd47032
Daniel Kweon <danielkweon19@gmail.com>
2021-07-27 17:48:33 -0500

    linked json data to tables

--- src/views/YouthGroup.vue
+++ src/views/YouthGroup.vue
@@ -1,36 +1,20 @@
 <template>
   <div class="root">
     <h1>Youth Group</h1>
-    <TableBox :array1="arrayBox" />
+    <TableBox :array1="dataJson" />
   </div>
 </template>
 
 <script>
 import TableBox from "../components/TableBox.vue"
+import dataBase from "../../data/db.json";
 export default {
   components:{TableBox},
   data() {
     return {
-      arrayBox: [
-        {
-          id: "1",
-          date: "01/03",
-          title: "Daily Bible Study",
-          pdf: "",
-          sermon:
-            "https://www.youtube.com/watch?v=3fyTOiK29nM&list=PLWj53mXE8Z2SGZVwNEZLbWkllo1KrC4lv&index=1&ab_channel=JBChurchSchool",
-        },
-        {
-          id: "2",
-          date: "01/10",
-          title: "In This Manner Pray",
-          pdf: "",
-          sermon:
-            "https://www.youtube.com/watch?v=swizswbN8zw&list=PLWj53mXE8Z2SGZVwNEZLbWkllo1KrC4lv&index=3&ab_channel=JBChurchSchool",
-        },
-      ],
+      dataJson: dataBase.lessons.year_2021.youth_group,
     };
-  },
+  }
 };
 </script>
 
