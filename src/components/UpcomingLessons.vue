25c05c3d5899c8c69c27c5635fef394f12b70aea
Ezra Freeman <ezra.jbch@gmail.com>
2021-07-30 01:31:20 -0500

    made a basic bootstrap grid to start

---
+++ src/components/UpcomingLessons.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="container my-5"></div>
+  <div class="row my-2 mx-0">
+    <div class="p-5 light-blue">Upcoming Lessons</div>
+  </div>
+  <div class="row">
+    <div class="col-lg-4 my-2">
+      <div class="p-5 light-blue">This Week Kindergarten</div>
+    </div>
+    <div class="col-lg-4 my-2">
+      <div class="p-5 light-blue">This Week Elementary</div>
+    </div>
+    <div class="col-lg-4 my-2">
+      <div class="p-5 light-blue">This Week YG</div>
+    </div>
+  </div>
+</template>
+
+<style>
+.light-blue {
+  background: #b3e6ff;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
     <h1>Church School Website</h1>
+    <UpcomingLessons />
   </div>
 </template>
 
 <script>
+import UpcomingLessons from '../components/UpcomingLessons.vue'
 
 export default {
   name: 'Home',
   
-  components: {}
+  components: { UpcomingLessons }
 }
 
 </script>
