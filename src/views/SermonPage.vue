14ee985c9ba16250ae7a44812d15d950a429e461
Daniel Kweon <danielkweon19@gmail.com>
2021-08-20 00:20:08 -0500

    fixed SermonPage even more

--- src/views/SermonPage.vue
+++ src/views/SermonPage.vue
@@ -1,18 +1,21 @@
 <template>
-  <iframe
-    width="500"
-    height="600"
-    loading="lazy"
-    :src="getVideoURLForLessons()"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen
-  ></iframe>
+  <div>
+    <!--Not sure why, comments outside of this div breaks the component...-->
+    <!--Youtube links must have the "embed" attribute in the url for it to work properly. You get this from the "share" button on youtube.-->
+    <iframe
+      width="500"
+      height="600"
+      loading="lazy"
+      :src="getVideoURLForLessons()"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
 </template>
 
 <script>
-/* Youtube links must have the "embed" attribute in the url for it to work properly. You get this from the "share" button on youtube.*/
 export default {
   props: ["group", "year", "title"],
 
@@ -31,8 +34,8 @@ export default {
     getVideoURLForLessons() {
       /*Given the group and title of the lesson, go through the json file and find the url for the video*/
       for (let i = 0; i < this.cycles.length; i++) {
-        for(let j = 0; j<this.cycles[i].length; j++){
-          if(this.cycles[i][j].title == this.title){
+        for (let j = 0; j < this.cycles[i].length; j++) {
+          if (this.cycles[i][j].title == this.title) {
             return this.cycles[i][j].sermon;
           }
         }
